8d6140e3189c7fba72854d5157fb451aeb2dfe10
triblanc <tristan.blanc@uca.fr>
2026-05-20 08:13:07 +0200

    MAJ scripts tampon
    -> MAJ script script_coc_mcc.sql
    -> MAJ script script_suppression_chc_superflus

--- scripts/script_suppression_chc_superflus.sql
+++ scripts/script_suppression_chc_superflus.sql
@@ -9,21 +9,21 @@ declare
 										(
 											select code_apprenant,
 													 code_objet_formation,
-													 operation,
+													 type_choix_pedagogique,
 													 code_chemin,
 													 code_periode,
-													 code_formation
+													 code_filtre_formation
 											from apprenant_chc ac 
 										)
-										select tab2.code_apprenant ||'-' || tab2.code_objet_formation || '-' || tab2.code_periode || '-' ||tab2.code_formation id	 
+										select tab2.code_apprenant ||'-' || tab2.code_objet_formation || '-' || tab2.code_periode || '-' ||tab2.code_filtre_formation id	 
 										from tab tab1,
 											  tab tab2
 										where tab1.code_apprenant = tab2.code_apprenant 
 										and tab1.code_objet_formation <> tab2.code_objet_formation 
 										and tab1.code_periode = tab2.code_periode
-										and tab1.operation = tab2.operation
-										and tab1.code_formation = tab2.code_formation 
-										and tab1.operation = 'AM' 
+										and tab1.type_choix_pedagogique = tab2.type_choix_pedagogique
+										and tab1.code_filtre_formation = tab2.code_filtre_formation 
+										and tab1.type_choix_pedagogique = 'AM' 
 										and tab2.code_chemin like  '%' || tab1.code_chemin ||'%'
 									) resultat;
 
@@ -35,8 +35,8 @@ declare
 											where exists 
 											(
 												select 1
-												from apprenant_chc chc where chc.code_periode = ac.code_periode and chc.code_formation <>ac.code_formation  and chc.code_chemin = ac.code_chemin and ac.code_apprenant = chc.code_apprenant  and chc.code_objet_formation = ac.code_objet_formation 
-											) and operation <> 'AM';
+												from apprenant_chc chc where chc.code_periode = ac.code_periode and chc.code_filtre_formation <>ac.code_filtre_formation  and chc.code_chemin = ac.code_chemin and ac.code_apprenant = chc.code_apprenant  and chc.code_objet_formation = ac.code_objet_formation 
+											) and type_choix_pedagogique <> 'AM';
 
 		
 -- resolution du probleme "pere sans chc"
@@ -58,7 +58,7 @@ begin
 		BEGIN
 
 			DELETE FROM apprenant_chc appchc
-			WHERE  appchc.code_apprenant ||'-'||appchc.code_objet_formation || '-' || appchc.code_periode || '-' || appchc.code_formation = rec_element_fils.id;	
+			WHERE  appchc.code_apprenant ||'-'||appchc.code_objet_formation || '-' || appchc.code_periode || '-' || appchc.code_filtre_formation = rec_element_fils.id;	
 			RAISE NOTICE ' ---> Suppression CHC';
 			
 		EXCEPTION
